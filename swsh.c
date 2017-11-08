01b9bc3d39ac3f2ff65c517ef62c5f33b7cb1b1b
gold9598 <gold9598@gmail.com>
2017-11-08 13:32:23 +0900

    simple shell v1.0

--- swsh.c
+++ swsh.c
@@ -12,12 +12,17 @@
 //-----------------------------------------------------------
 
 #include <stdlib.h>
-
+#include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-
+#include <stdio.h> // Because of the function named rename
+#include <signal.h>
 #include "string_sw.h"
+void handler(int sig)
+{
+	if(sig==SIGINT||sig == SIGTSTP)	write(2,"\n",1);
+}
 int cmd_typechk(char *tmp)
 {
 	char head[201];
@@ -59,118 +64,248 @@ void print_prompt()
     strcpy(head, "swsh:");
     strcat(head, ptr);
     strcat(head, "> ");
-    ret = write(2, head, strlen(head));
+    ret = write(0, head, strlen(head));
 
     if (ret <= 0)
         exit(1);
 }
-void cmd_exec(char *input,int *pc,int *cur_pid)
+void cmd_exec(char *input)
 {
-	char tmp[200]; // Initialize character array...
-	int i;
-	for(i=0;i<200;i++)	tmp[i]='\0';
-	i=0;
+	char X[200];
+	int pipenum=0;
+	int input_exist=0;
+	int output_exist=0;
+	int i=0;
 	while(input[i]!='\n')
 	{
-		tmp[i]=input[i];
+		X[i]=input[i];
+		if(input[i]=='<')	input_exist++;
+		if(input[i]=='>')	output_exist++;
+		if(input[i]=='|')	pipenum++;
 		i++;
 	}
-	tmp[i]='\n';
-	while(*input!='|'&&*input!='\n')	input++;
-	if(*input=='|')
+	
+	char *str = X;
+	/* breaking out input & output */
+	char* out = NULL;
+	if(input_exist)
 	{
-		input++;
-		while(*input==' ')	input++;
-	}
-	for(i=0;i<200;i++)	if(tmp[i]==' ')	tmp[i]='\0';	
-	char *x = tmp;
-	while(*x!='\n'&&*x!='|')	x++;
-	int block = 1;
-	int pip =0;
-	if(*x=='|')	pip = 1;	
-	int j=0;
-	if(*x=='|')	x--;
-	for(int i = 0 ; i <x-tmp ; i++)	if(tmp[i] == '\0') block++;
-	char* b[block+1];
-	b[j] = tmp;	j++;
-	for(int i = 0 ; i <x-tmp ; i++)
-	{
-		if(tmp[i] == '\0'&&tmp[i+1]!='|'&&tmp[i+1]!='>')
+		char *p = str;
+		while(*p!='<')	p++;
+		*p = ' ';
+		/*
+		char *s = p;
+		s++;
+		while(*s==' ')	s++;
+		char *z = s;
+		while(*z!=' ')	z++;
+		int len = z-s;
+		int in_x[len+1];
+		for(i=0;i<len;i++)	in_x[i]=*(s+i);
+		in_x[i+1]='\0';
+		in = in_x;
+		while(p!=s)
 		{
-			b[j] = tmp+i+1;
-			j++;
+			*p='\0';
+			p++;
+		}
+		*/
+	}
+	if(output_exist)
+	{
+		char *p = str;
+		while(*p!='>')	p++;
+		char *s = p;
+		while(*s=='>')
+		{
+			*s = '\n';
+			s++;
+		}
+		while(*s==' ')	s++;
+		out = s;
+		while(*s!='\n'&&*s!=' ')	s++;
+		while(*s==' ')
+		{
+			*s='\0';
+			s++;
+		}
+		if(*s == '\n')	*s='\0';
+		*p='\n';
+	}
+	/* data structure declaration */
+	X[i]='\n';
+	char** b[pipenum+1]; // Instruction block.
+	int p[pipenum][2]; // pipe.
+	/* tokenization */
+	for(i=0;i<pipenum+1;i++)
+	{
+		char *x = str;
+		int inst=0;
+		while(*x!='\n'&&*x!='|')
+		{
+			if(*x!=' ')
+			{
+				inst++;
+				while(*x!=' '&&*x!='\n'&&*x!='|')	x++;
+			}
+			else	while(*x==' ')	x++;
+		}
+		b[i] = (char**)malloc(sizeof(char*)*(inst+1));
+		b[i][inst]=NULL;
+		for(int j=0;j<inst;j++)
+		{
+			if(j==0)	b[i][j]=str;
+			else
+			{
+				while(*str!=' ')	str++;
+				while(*str==' ')
+				{
+					*str='\0';
+					str++;
+				}
+				b[i][j]=str;
+			}
+		}
+		while(*str!='|'&&*str!='\n')
+		{
+			if(*str==' ')	*str='\0';
+			str++;
+		}
+		if(*str=='|')
+		{
+			*str = '\0';
+			str++;
+			while(*str==' ')	str++;
+		}
+		if(*str=='\n')	*str='\0';
+	}
+	/*output file handling */
+	if(output_exist)
+	{	
+		char *o = out;
+		while(*o != '\n' && *o != ' ' && *o != '\0')	o++;
+		*o = '\0';
+	}
+	/* exception handling stage */
+	if(input_exist && strcmp(b[0][0],"cat")!=0)
+	{
+		int last=0;
+		while(b[0][last]!=NULL)	last++;
+		b[0][last-1]=NULL;	// nullify input option
+	}	
+	if(strcmp(b[0][0],"exit")==0) // in case of exit mode.
+	{
+		if(pipenum == 0)
+		{
+			if(b[0][1]==NULL)	exit(0);
+			else if('0'<=*b[0][1]&&*b[0][1]<='9')	exit(*b[0][0]-'0');
+		}
+		else	write(2,"swsh: Command not found\n",24);
+		return;
+	}
+	if(strcmp(b[0][0],"cd") ==0) // Change directory
+	{
+		if( pipenum == 0)
+		{
+			if(b[0][1]!=NULL)
+			{
+				chdir(b[0][1]);	
+			}
+			else	chdir(getenv("HOME"));
+		}
+		return;
+	}
+	if(strcmp(b[0][0],"rm")==0) // If rm with no option, proceed this.
+	{
+		if(b[0][2]==NULL)
+		{
+			if(-1==unlink(b[0][1]))
+			{
+				write(2,"Error occurred: ",16);
+			}
+			return;
 		}
 	}
-	b[block]=NULL;
-	if(cmd_typechk(b[0])==-1 && !(b[0][0]=='.'&&b[0][1]=='/') )
+	if(strcmp(b[0][0],"mv")==0)
 	{
-		if(b[0][0]!='\n')	write(2,"swsh: Command not found\n",24);
+		if(-1==rename(b[0][1],b[0][2]))
+		{
+			write(2,"Error occurred: ",16);
+		}
+		return;	
+	}
+	if(strcmp(b[0][0],"pwd")==0)
+	{
+		char cur[255];
+		getcwd(cur,255);
+		write(0,cur,strlen(cur));
+		write(0,"\n",1);
 		return;
 	}
-	if(*x=='\n'||*x==' ')	*x='\0';
-	if(strcmp(b[0],"cd")==0&&pip==0)
-	{
-		chdir(b[1]);
-		return;
-	}
+	/* execution stage */
 	pid_t pid;
-	int dc[2];
-	int status;
-	if((pid=fork())==0)
+	int fd;
+	if(output_exist==1)	fd = open(out,O_RDWR|O_CREAT|O_TRUNC,0666);
+	if(output_exist==2)	fd = open(out,O_RDWR|O_APPEND);
+	for(i=0;i<pipenum;i++)	pipe(p[i]);
+	for(i=0;i<pipenum+1;i++)
 	{
-		if(pc!=NULL)
+		if((pid=fork())==0)
 		{
-			dup2(pc[0],0);
-			close(pc[1]);
+			if(strcmp(b[i][0],"man")&&strcmp(b[i][0],"vim"))
+			{
+				if(setpgid(pid,pid)<0)	exit(0);
+			}
+			if(i>0)
+			{
+				if(dup2(p[i-1][0],0)<0)	exit(0);
+			}
+			if(i<pipenum)
+			{
+				if(dup2(p[i][1],1)<0)	exit(0);
+			}
+			if(i==pipenum&&output_exist)
+			{
+				dup2(fd,1);
+			}
+			for(int j=0;j<pipenum;j++)
+			{
+				close(p[j][0]);	close(p[j][1]);
+			}
+			if(execvp(b[i][0],b[i])<0)
+			{
+				write(2,"swsh: Command not found\n",24);
+				exit(0);
+			}
+			exit(0);
 		}
-		if(pip)
-		{
-			pipe(dc);
-			dup2(dc[1],1);
-			close(dc[0]);
-		}
-		if(execvp(b[0],b)<0)
-		{
-			write(2,"swsh: Command not found\n",24);
-			exit(1);
-		}
-		else	close(pc[0]);
-		exit(1);
 	}
-	else
+	for(i=0;i<pipenum;i++)
 	{
-		*cur_pid = pid;
-		if(pip)
-		{
-			cmd_exec(input,dc,cur_pid);
-		}
-		*cur_pid = pid;
-		return;
+		close(p[i][0]);	close(p[i][1]);
 	}
+	if(output_exist)	close(fd);
 }
 int main(void)
 {
-    pid_t cur_pid = -1;
+    int status;
+    signal(SIGINT,handler);
+    signal(SIGTSTP,handler);
     int ret;
     char input[240];
     while (1) {
-        if(cur_pid!=-1)
-	{
-		int status;
-		waitpid(cur_pid,&status,0);
-		print_prompt();
-		cur_pid=-1;
-	}
-	else	print_prompt();
+	pid_t pid_x = wait(&status);
+	print_prompt();
 	for(int i=0;i<240;i++)	input[i]='\0';
-        ret = read(0, input, 200);
-	cmd_exec(input,NULL,&cur_pid);
+	ret = read(0, input, 200);
+	if(input[0]!='\n')	cmd_exec(input);
 	if (ret < 0)
             exit(1);
         else if (ret == 0) {
             write(2, "exit\n", 5);
             break;
         }
+        //write(2, input, ret);
     }
 
     return 0;
